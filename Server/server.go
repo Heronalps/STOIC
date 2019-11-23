c71849c58a395e52047df7c6dee12299466559a2
Michael Zhang <heronalps@gmail.com>
2019-11-22 18:11:55 -0800

    mz - Update GetBandWidth

--- Server/server.go
+++ Server/server.go
@@ -5,27 +5,38 @@
 package server
 
 import (
+	"bufio"
 	"fmt"
 	"os/exec"
 	"os/user"
+	"strconv"
+	"strings"
 )
 
 /*
-TailHTTP runs background cron job.
+GetBandWidth solicits bandwidth of Pi Zero at Sedgwick Reserve.
 */
-func TailHTTP() {
+func GetBandWidth() float64 {
 	url := "http://169.231.235.221/sedgtomayhem.txt"
-	htailPath := HomeDir() + "/Downloads/Github/htail/htail.py"
-	command := "python2"
+	lines := "1"
 	fmt.Printf("Tailing http endpoint : %s\n", url)
 
-	cmd := exec.Command(command, htailPath, url)
-	out, err := cmd.Output()
+	cmd := "curl " + url + " | tail -" + lines
+	out, err := exec.Command("bash", "-c", cmd).Output()
 	if err != nil {
 		println(err.Error())
-		return
+		return 0
 	}
-	fmt.Println(string(out))
+
+	scanner := bufio.NewScanner(strings.NewReader(string(out)))
+	var lastLine string
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+		lastLine = scanner.Text()
+	}
+	fields := strings.Fields(lastLine)
+	bandWidth, err := strconv.ParseFloat(fields[0], 64)
+	return bandWidth
 }
 
 /*
