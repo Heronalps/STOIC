d5927b313d2bd83aebdbcf7fab52f06757a14f53
Michael Zhang <heronalps@gmail.com>
2019-12-04 00:19:13 -0800

    mz - add experiment script

--- StartServer.sh
+++ StartServer.sh
@@ -1,1 +1,5 @@
-go run main.go -node=server -ip=127.0.0.1 -port=5001 -image=10 -runtime=edge -batch=24 | tee -a server_log.txt
+go run main.go -node=server -ip=128.111.45.118 -port=5001 -image=20 -runtime=edge -batch=10 | tee -a edge_log.txt
+go run main.go -node=server -ip=128.111.45.118 -port=5001 -image=20 -runtime=cpu -batch=10 | tee -a cpu_log.txt
+go run main.go -node=server -ip=128.111.45.118 -port=5001 -image=20 -runtime=gpu1 -batch=10 | tee -a gpu1_log.txt
+go run main.go -node=server -ip=128.111.45.118 -port=5001 -image=20 -runtime=gpu2 -batch=10 | tee -a gpu2_log.txt
+go run main.go -node=server -ip=128.111.45.118 -port=5001 -image=20 -batch=10 | tee -a stoic_log.txt
