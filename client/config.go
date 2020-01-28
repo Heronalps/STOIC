baafdcb3ad490d7f41e33694d0e87a000bc50dea
Michael Zhang <heronalps@gmail.com>
2020-01-28 14:36:45 -0800

    mz - Modify runtimes

--- client/config.go
+++ client/config.go
@@ -10,9 +10,10 @@ var (
 	namespace           string = "racelab"
 	RunDeployment       string = "image-clf-inf"
 	timeQueryDeployment string = "image-clf-inf37"
-	runtimes                   = []string{"edge", "cpu", "gpu1", "gpu2"}
-	NautilusRuntimes           = []string{"cpu", "gpu1", "gpu2"}
-	setupImageNums      []int  = []int{33, 10}
+	// runtimes                   = []string{"edge", "cpu", "gpu1", "gpu2"}
+	runtimes               = []string{"gpu1", "gpu2"}
+	NautilusRuntimes       = []string{"cpu", "gpu1", "gpu2"}
+	setupImageNums   []int = []int{33, 10}
 	// currentRuntime      string = "cpu" // cpu / gpu1 / gpu2
 	procTimeNumDP        int    = 10
 	serviceAccountConfig string = "KUBECONFIG=~/.kube/service-account"
