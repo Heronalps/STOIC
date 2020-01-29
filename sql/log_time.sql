5052dd8ecafdcfa60766cbca71a9f210b76b3f09
Michael Zhang <heronalps@gmail.com>
2020-01-28 21:10:29 -0800

    mz - add gpu_analysis

--- sql/log_time.sql
+++ sql/log_time.sql
@@ -15,8 +15,8 @@ INTO OUTFILE '/var/lib/mysql-files/logTime_4runtimes.csv' FIELDS TERMINATED BY '
 SELECT 'epoch', 'image_num', 'runtime', 'pred_total', 'pred_transfer', 'pred_deploy', 'pred_proc', 'act_total', 'act_transfer', 'act_deploy', 'act_proc'
 UNION
 SELECT UNIX_TIMESTAMP(time_stamp) as epoch, image_num, runtime, pred_total, pred_transfer, pred_deploy, pred_proc,
-act_total, act_transfer, act_deploy, act_proc from LogTime where task_id > 1350
-INTO OUTFILE '/var/lib/mysql-files/logTime_stoic.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
+act_total, act_transfer, act_deploy, act_proc from LogTime where task_id > 3138
+INTO OUTFILE '/var/lib/mysql-files/logTime_gpu.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
 
 select * from LogTime where task_id >= 512 and task_id < 1343;
 select * from LogTime where task_id > 1350;
