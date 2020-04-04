e2393f753e178c976f57b0eef76bb50da3a33ca5
Michael Zhang <heronalps@gmail.com>
2020-04-03 17:03:00 -0700

    mz - Add transfer.sh

---
+++ scripts/transfer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Parameter n - number of files to fetch in the directory
+
+# 1. ssh into remote server and list the files that need to fetch
+# 2. scp to local 
+
+path="/opt2/sedgwick/images/Main_timelapse/2016/11"
+target="/Users/michaelzhang/Downloads/WTB_samples/time_lapse"
+
+files=$(ssh heronalps@128.111.39.240 ls $path | tail -n $1)
+
+while IFS= read -r file; 
+do
+
+scp "heronalps@128.111.39.240:$path/$file" $target; 
+
+done <<< "$files"
