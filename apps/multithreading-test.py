f4f089a06ae7502dd824056e55a603d0ba33528a
Michael Zhang <heronalps@gmail.com>
2020-06-16 10:44:12 -0700

    mz - time ctime

--- apps/multithreading-test.py
+++ apps/multithreading-test.py
@@ -1,12 +1,12 @@
 from multiprocessing import Pool
-import time, datetime
+import time
 
 def f(x):
     start = time.time()
     while (True):
         curr = time.time()
         if (curr - start >= 300):
-            print ("Epoch : " + str(datetime.datetime(curr)))
+            print ("Epoch : " + str(time.ctime(curr)))
             break
         r = pow(x, x)
 
